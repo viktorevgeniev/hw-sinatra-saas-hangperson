f72a7db028af62a63f6c7bae3a64b4fc3fc495e2
Viktor Evgeniev <viktor.evgeniev@gmail.com>
2015-11-07 17:46:53 +0000

    Finished HangpersonGame class

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,3 +1,5 @@
+require 'active_support/all' 
+
 class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
@@ -8,8 +10,55 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses, :valid
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+  
+
+  
+  def guess(letter)
+    if(letter =~ /[a-z]/i)
+      valid = letter.downcase
+    elsif(letter.nil? || letter =~ /^\s*.*$/)
+      raise ArgumentError.new("Invalid letter")
+    end
+    if word.include?(valid)
+      if guesses.exclude?(valid)
+        guesses << valid
+      else
+        return false
+      end
+    else
+      if wrong_guesses.exclude?(valid)
+        wrong_guesses << valid
+      else
+      return false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    result = ''
+    
+    word.split('').each do |letter|
+      if guesses.include?(letter)
+        result << letter
+      else
+        result << '-'
+      end
+    end
+    
+    result
+  end
+  
+  def check_win_or_lose
+    return :lose if wrong_guesses.length >= 7
+    return :win unless word_with_guesses.include?('-')
+    :play
   end
 
   def self.get_random_word
